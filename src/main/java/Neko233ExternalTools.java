41b327e5fa71e245638a6b3c27f5064529a94afa
SolarisNeko <1417015340@qq.com>
2022-12-04 21:28:05 +0800

    init.

---
+++ src/main/java/Neko233ExternalTools.java
@@ -0,0 +1,10 @@
+/**
+ * @author SolarisNeko
+ * Date on 2022-12-04
+ */
+public class Neko233ExternalTools {
+
+    public static String readMe() {
+        return "just a dependency import project. nothing.";
+    }
+}
